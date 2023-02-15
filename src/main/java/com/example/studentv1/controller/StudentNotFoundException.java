8e5c3f27d16b4331d82a74b9cbe7240d1bf3f19d
benj302g <benj302g@stud.kea.dk>
2023-02-15 10:48:55 +0100

    test

--- src/main/java/com/example/studentv1/controller/StudentNotFoundException.java
+++ src/main/java/com/example/studentv1/controller/StudentNotFoundException.java
@@ -3,6 +3,6 @@ package com.example.studentv1.controller;
 public class StudentNotFoundException extends RuntimeException{
     public StudentNotFoundException(String name){
         super("Student not found: " + name);
-        //
+        //asda
     }
 }
